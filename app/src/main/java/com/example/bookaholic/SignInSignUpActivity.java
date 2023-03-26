a6243e195650084316742bc3272d48adab074ff4
shinkyung <ngogiahai014@gmail.com>
2023-03-26 22:18:16 +0700

    build UI register

--- app/src/main/java/com/example/bookaholic/SignInSignUpActivity.java
+++ app/src/main/java/com/example/bookaholic/SignInSignUpActivity.java
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class SignInSignUpActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
     }
 }
