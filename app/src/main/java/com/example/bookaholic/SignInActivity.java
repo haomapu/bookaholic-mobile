4036a6ac44b93b2a3dce5ef3bcaaf1d6113e4419
shinkyung <ngogiahai014@gmail.com>
2023-03-31 15:59:51 +0700

    link UI and add button

--- app/src/main/java/com/example/bookaholic/SignInActivity.java
+++ app/src/main/java/com/example/bookaholic/SignInActivity.java
@@ -22,12 +22,12 @@ public class SignInActivity extends AppCompatActivity{
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_book);
+        setContentView(R.layout.activity_login);
 
-//        btnLogin = findViewById(R.id.btnLogin);
-//        emailLogin = findViewById(R.id.editEmailLogin);
-//        passLogin = findViewById(R.id.editPasswordLogin);
-//        btnSignUp = findViewById(R.id.btnSignUp);
+        btnLogin = findViewById(R.id.btnLogin);
+        emailLogin = findViewById(R.id.editEmailLogin);
+        passLogin = findViewById(R.id.editPasswordLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,7 +42,7 @@ public class SignInActivity extends AppCompatActivity{
                     showToast(SignInActivity.this, R.string.empty_text);
 //                valid với firebase ở đây
                 else if (email.equals("giahai") && password.equals("123")) {
-                    Intent intent = new Intent(SignInActivity.this, Detail.class);
+                    Intent intent = new Intent(SignInActivity.this, MainActivity.class);
                     startActivity(intent);
                 }
                 else
